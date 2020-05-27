4dbb7352d062b8b73d028aabb6a74948b0df90a1
Joshua MacSween <joshmacsween@gmail.com>
2020-05-27 16:47:39 -0600

    added font, styling to search bar

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,19 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="search..." />
       </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Edmonton, AB</div>
+          <div class="date">May 27, 2020</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temperature">10°</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
+
     </main>
   </div>
 </template>
@@ -43,5 +56,36 @@ export default {
     padding: 25px;
     background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
     }
+
+  .search-box {
+    width: 70%;
+    margin-bottom: 30px;
+    margin-left: 15%;
+  }
+
+  .search-box .search-bar {
+    width: 100%;
+    display: block;
+    padding: 15px;
+    color: #313131;
+    font-size: 20px;
+
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.5);
+    border-radius: 0px 16px 0px 16px;
+    transition: 0.4s;
+  }
+
+  .search-box .search-bar:focus {
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+    background-color: rgba(255, 255, 255, 0.75);
+    border-radius: 16px 0px 16px 0px;
+  }
+
+
 </style>
 
